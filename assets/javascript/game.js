10ec352167e3d17377c9b797994c19dfe5f2b1c6
Thuy Nguyen <thuynguyen.nht89@gmail.com>
2020-02-04 21:13:41 -0800

    add code for basic game play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,10 @@ var wins = 0;
 var losses = 0;
 var totalGuess = 9;
 
+var init = function () {
+    totalGuess = 9;
+};
+
 document.onkeyup = function (event) {
     //user chooses a letter by clicking 1 alphabet key.
     var userGuesses = event.key.toLocaleLowerCase();
@@ -19,51 +23,29 @@ document.onkeyup = function (event) {
     //computer random chooses 1 alphabet key.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+    alert("User guess: " + userGuesses);
+    alert("Computer guess: " + computerGuess)
     //checks to see if the letter match. if the letter match, wins adds 1. Then the game refresh.
     if (userGuesses === computerGuess) {
         console.log("You wins");
         wins++;
     }
-    alert("User guess: " + userGuesses);
-    alert("Computer guess: " + computerGuess)
-
     //if the letter not match, guessturn -1. Users can continuing chossing until guessturn = 0,
+    if (userGuesses !== computerGuess) {
+        totalGuess--;
+    }
+
+    if (totalGuess === 0) {
+        losses++;
+        init();
+    }
+
 
 
     //if guessturn =0, adds to looses. Game refresh.
 
     //call out the variable and replace variable by something
     winsText.textContent = wins;
+    guessTurns.textContent = totalGuess;
+    lossesText.textContent = losses;
 };
-
-// for (i = 9; i > 0; i--) {
-//     document.onkeyup = function (event) {
-
-
-
-
-
-//         console.log(i);
-//         guessTurns.textContent = i;
-
-
-
-//         if (userGuesses !== computerGuess) {
-
-//             console.log("You lost")
-
-//         } else {
-//             console.log("You wins");
-//         }
-
-//         console.log(userGuesses);
-
-//         guessesText.textContent = userGuesses;
-//         console.log(computerGuess);
-//     }
-
-// };
-
-
-// var userGuessesArray = [];
-// var userGuesses;
